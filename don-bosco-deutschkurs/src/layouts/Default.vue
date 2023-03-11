84c034a16918953f4f3e165ea6e098bad4a8b1ff
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 22:11:50 +0100

    update Default with component BackToTopArrow

--- don-bosco-deutschkurs/src/layouts/Default.vue
+++ don-bosco-deutschkurs/src/layouts/Default.vue
@@ -5,18 +5,21 @@
       <slot />
     </main>    
     <Footer />
+    <BackToTopArrow />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
 import Footer from  '../components/Footer.vue';
+import BackToTopArrow from '../components/BackToTopArrow.vue';
 
 export default {
   components: {
     Header,
     Footer,
-  },
+    BackToTopArrow
+},
 };
 </script>
 
