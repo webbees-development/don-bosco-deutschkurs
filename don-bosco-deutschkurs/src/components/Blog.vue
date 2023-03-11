241df933b11db8766269d0377bcd98eaffb7b840
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 22:15:25 +0100

    add Blog, not finished

---
+++ don-bosco-deutschkurs/src/components/Blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="blog" class="blog">
+    <h2>Erfolgsgeschichten & Worte der Dankbarkeit</h2>
+
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/global.scss";
+h2 {
+  padding-block-end: 3.125rem;
+}
+.blog {
+  background-image: url("../assets/images/index/hintergrund.png");
+  padding-block: 9rem;
+  padding-inline: 15.8rem;
+  @media (max-width: 1250px) {
+    padding-inline: var(--padding-global-inline);
+  }
+  text-align: center;
+}
+</style>
