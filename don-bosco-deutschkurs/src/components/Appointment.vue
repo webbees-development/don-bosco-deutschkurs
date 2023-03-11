104d45a5772f96b2bd91850745b2455e98d38a9e
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 21:34:24 +0100

    add component Appointment

---
+++ don-bosco-deutschkurs/src/components/Appointment.vue
@@ -0,0 +1,98 @@
+<template>
+  <section class="appointment">
+    <div class="content">
+      <h2>
+        Tu anderen und<br />
+        damit auch dir selbst etwas Gutes.
+      </h2>
+      <p>
+        Nimm dir 2 Stunden, teile deine Sprache Deutsch und bereichere damit
+        (d)ein ganzes Leben.
+      </p>
+      <g-link class="btn" :to="$static.metadata.calendly_url"
+        >Probetermin vereinbaren</g-link
+      >
+    </div>
+    <div class="image">
+      <g-image src="../assets/images/image-gallery/haende.jpg" />
+    </div>
+  </section>
+</template>
+
+<static-query>
+query {
+  metadata {
+    calendly_url
+  }
+}
+</static-query>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/global.scss";
+.appointment {
+  background-image: url("../assets/images/index/hintergrund.png");
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-block: 3.75rem;
+}
+.content {
+  padding-inline-end: min(10.5vw, 12.5rem);
+  @media (max-width: $first-break) {
+    padding-inline-end: 8vw;
+  }
+}
+h2 {
+  max-width: 16ch;
+}
+p,
+a {
+  margin-inline-start: 2.5rem;
+}
+p {
+  margin-block: 3.25rem 2.5rem;
+  max-width: 28ch;
+}
+.image {
+  width: min(50vw, 61rem);
+  aspect-ratio: 1.3;
+  object-fit: cover;
+  @media (max-width: 1600px) {
+    aspect-ratio: 1.2;
+  }
+  @media (max-width: 1400px) {
+    aspect-ratio: 1.3;
+  }
+}
+@media (max-width: $tablet-break) {
+  .appointment {
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding-block: 7rem;
+    padding-inline: var(--padding-global-inline);
+  }
+  .content {
+    padding: 0;
+  }
+  h2 {
+    max-width: 20ch;
+    margin-inline: auto;
+    br {
+      display: none;
+    }
+  }
+  p,
+  a {
+    margin-inline-start: 0;
+  }
+  p {
+    margin-block: 2.8rem 2rem;
+    max-width: 40ch;
+  }
+  .image {
+    display: none;
+  }
+}
+</style>
