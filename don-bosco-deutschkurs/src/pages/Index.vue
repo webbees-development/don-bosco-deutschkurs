8953381cbeb51b0cc1c9d1188998aeef2f528215
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 22:15:38 +0100

    update Index with component Blog

--- don-bosco-deutschkurs/src/pages/Index.vue
+++ don-bosco-deutschkurs/src/pages/Index.vue
@@ -4,6 +4,7 @@
     <AboutUs/>
     <Appointment />
     <Participate />
+    <Blog />
     <Photos />
     <Testimonial />
     <Ending />
@@ -15,6 +16,7 @@ import Hero from '../components/Hero.vue';
 import AboutUs from '../components/AboutUs.vue';
 import Appointment from  '../components/Appointment.vue';
 import Participate from  '../components/Participate.vue';
+import Blog from '../components/Blog.vue';
 import Photos from '../components/Photos.vue';
 import Testimonial from '../components/Testimonial.vue';
 import Ending from '../components/Ending.vue';
@@ -28,6 +30,7 @@ export default {
     AboutUs,
     Appointment,
     Participate,
+    Blog,
     Photos,
     Testimonial,
     Ending
