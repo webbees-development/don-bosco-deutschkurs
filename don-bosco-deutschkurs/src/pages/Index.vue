050f9638ec16125a962f794b0e28d033201040e7
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 21:25:45 +0100

    update Index with AboutUs component

--- don-bosco-deutschkurs/src/pages/Index.vue
+++ don-bosco-deutschkurs/src/pages/Index.vue
@@ -1,17 +1,20 @@
 <template>
   <Layout>
     <Hero />
+    <AboutUs/>
   </Layout>
 </template>
 
 <script>
 import Hero from '../components/Hero.vue';
+import AboutUs from '../components/AboutUs.vue';
 export default {
   metaInfo: {
     title: "Don Bosco für Flüchtlinge",
   },
   components:{
     Hero,
+    AboutUs
   }
 }
 </script>
