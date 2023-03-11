9bbedddf2eb53c1045e5fa9c8d0938f2c7e80bff
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-11 21:34:47 +0100

    update Index with Appointment component

--- don-bosco-deutschkurs/src/pages/Index.vue
+++ don-bosco-deutschkurs/src/pages/Index.vue
@@ -2,19 +2,22 @@
   <Layout>
     <Hero />
     <AboutUs/>
+    <Appointment />
   </Layout>
 </template>
 
 <script>
 import Hero from '../components/Hero.vue';
 import AboutUs from '../components/AboutUs.vue';
+import Appointment from  '../components/Appointment.vue';
 export default {
   metaInfo: {
     title: "Don Bosco für Flüchtlinge",
   },
   components:{
     Hero,
-    AboutUs
+    AboutUs,
+    Appointment
   }
 }
 </script>
