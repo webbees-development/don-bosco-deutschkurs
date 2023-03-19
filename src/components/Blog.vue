d8f9240342721c86f081525dfbdc3a81d9cad4d7
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-19 19:11:48 +0100

    Update Blog.vue
    add blog section

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,22 +1,79 @@
 <template>
   <section id="blog" class="blog">
     <h2>Erfolgsgeschichten & Worte der Dankbarkeit</h2>
-
+    <GenericSlider>
+      <PostPreview
+        v-for="{ node } in $static.posts.edges"
+        :key="node.id"
+        class="slide generic-slide"
+        :link="`/${node.id}`"
+        :image="node.featured_image"
+        :title="node.title"
+        :excerpt="node.excerpt"
+        :white_border="true"
+      />
+    </GenericSlider>
+    <g-link class="btn" to="/blog">zu allen Beiträgen</g-link>
   </section>
 </template>
 
+<static-query>
+  query {
+    posts: allPost(perPage: 10, filter: { published: { eq: true } }) {
+      edges {
+        node {
+          id
+          title
+          featured_image {
+            src
+            alt
+          }
+          excerpt
+        }
+      }
+    } 
+  }
+</static-query>
+
+<script>
+import GenericSlider from './GenericSlider.vue';
+import PostPreview from './PostPreview.vue';
+export default{
+  components:{
+    GenericSlider,
+    PostPreview
+  },
+}
+</script>
+
 <style lang="scss" scoped>
 @import "~/assets/styles/global.scss";
+
 h2 {
   padding-block-end: 3.125rem;
 }
-.blog {
+.blog { 
+  text-align: center;
+
   background-image: url("../assets/images/index/hintergrund.png");
   padding-block: 9rem;
-  padding-inline: 15.8rem;
-  @media (max-width: 1250px) {
-    padding-inline: var(--padding-global-inline);
+  padding-inline: min(11vw, 15rem);
+
+  --carousel-btn-spacing: -5.5rem;
+  --carousel-btn-upper-spacing: 16%;
+  --carousel-btn-color: var(--clr-primary);
+
+  @media (max-width: 1750px) {
+    --carousel-btn-spacing: -4.5rem;
+    --carousel-btn-upper-spacing: 14%;
   }
-  text-align: center;
+  @media (max-width: 1500px) {
+    //--carousel-btn-spacing: -3rem;
+    --carousel-btn-upper-spacing: 20%;
+  }
+
+}
+.btn{
+  margin-block-start: 5rem;
 }
 </style>
