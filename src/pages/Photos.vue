ceba60349b2abcdc8faaca283d19af79ed3841a5
stephan steiner <dev@webbees.at>
2023-03-19 15:54:27 +0100

    Update Photos.vue
    add meta data

--- src/pages/Photos.vue
+++ src/pages/Photos.vue
@@ -17,6 +17,19 @@
   </Layout>
 </template>
 
+<static-query>
+  query {
+    metadata {
+      siteName
+      siteDescription
+      siteUrl
+    }
+    customImage(id : "site-logo") {
+      src
+    }
+  }
+</static-query>
+
 <script>
 export default {
   data() {
@@ -39,6 +52,83 @@ export default {
         this.photos.push({ pathLong: r(key), pathShort: key })
       );
     },
+  },
+  metaInfo() {
+    const siteUrl = this.$static.metadata.siteUrl;
+    const pageTitle = 'Foto Gallerie | Freude erleben und teilen' +  ' | ' + this.$static.metadata.siteName;
+    const pageDescription = "";
+    const pageUrl = `${siteUrl}${this.$route.path}`;
+    const image = this.$static.customImage ? this.$static.customImage.src : undefined;
+    const imagePath = image ? `${siteUrl}${image.src}` : ''
+    return {
+      title: pageTitle.split('|')[0].trim(),
+      meta: [
+        { key: 'description', name: 'description', content: pageDescription },
+        { key: 'og:url', property: 'og:url', content: pageUrl },
+        {
+          key: 'og:title',
+          property: 'og:title',
+          content: pageTitle
+        },
+        {
+          key: 'og:type',
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          key: 'og:description',
+          property: 'og:description',
+          content: pageDescription
+        },
+        {
+          key: 'og:image',
+          property: 'og:image',
+          content: imagePath
+        },
+        {
+          key: 'og:image:width',
+          property: 'og:image:width',
+          content: image ? image.size.width : ''
+        },
+        {
+          key: 'og:image:height',
+          property: 'og:image:height',
+          content: image ? image.size.height : ''
+        },
+        {
+          key: 'twitter:description',
+          name: 'twitter:description',
+          content: pageDescription
+        },
+        {
+          key: 'twitter:card',
+          name: 'twitter:card',
+          content: image ? 'summary_large_image' : 'summary'
+        },
+        {
+          key: 'twitter:image',
+          property: 'twitter:image',
+          content: imagePath
+        },
+        {
+          key: 'twitter:title',
+          property: 'twitter:title',
+          content: pageTitle
+        }
+      ],
+      script: [
+        {
+          type: 'application/ld+json',
+          json: {
+            '@context': 'http://schema.org',
+            '@type': 'Website',
+            description: pageDescription,
+            headline: pageTitle,
+            image: imagePath
+          }
+        }
+      ],
+    }
   },
 };
 </script>
