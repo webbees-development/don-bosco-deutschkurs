adf0ce3e7be6a348540b65921146ff7ced1613b0
stephan steiner <dev@webbees.at>
2023-03-19 15:30:19 +0100

    Update Datenschutz.vue
    add content

--- src/pages/Datenschutz.vue
+++ src/pages/Datenschutz.vue
@@ -1,5 +1,95 @@
 <template>
   <Layout>
-    Datenschutz
+    <h1>Datenschutzerklärung</h1>
+
+    <div class="card">
+      <!-- Datenschutz -->
+      <h3>Allgemeine Hinweise</h3>
+      <p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p>
+      <p>Die Nutzung unserer Website ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.</p>
+      <p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
+
+      <!-- Server-Log-Files -->
+      <h3>Server-Log-Files</h3>
+      <p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log-Files, die Ihr Browser automatisch an uns übermittelt. Dies sind:</p>
+      <ul>
+        <li>Browsertyp und Browserversion</li>
+        <li>verwendetes Betriebssystem</li>
+        <li>Referrer URL</li>
+        <li>Hostname des zugreifenden Rechners</li>
+        <li>Uhrzeit der Serveranfrage</li>
+      </ul>
+      <p>Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu prüfen, wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.</p>
+
+      <!-- Adobe Fonts -->
+      <h3>Adobe Fonts</h3>
+      <p>Wir verwenden auf unserer Website Adobe Fonts, ein Webschriftarten-Hosting-Dienst. Dienstanbieter ist das amerikanische Unternehmen Adobe Inc. Für den europäischen Raum ist das irische Unternehmen Adobe Systems Software Ireland Companies, 4-6 Riverwalk, Citywest Business Campus, Dublin 24, Irland, verantwortlich.</p>
+      <p>Adobe verarbeitet Daten von Ihnen u.a. auch in den USA. Wir weisen darauf hin, dass nach Meinung des Europäischen Gerichtshofs derzeit kein angemessenes Schutzniveau für den Datentransfer in die USA besteht. Dies kann mit verschiedenen Risiken für die Rechtmäßigkeit und Sicherheit der Datenverarbeitung einhergehen.</p>
+      <p>Als Grundlage der Datenverarbeitung bei Empfängern mit Sitz in Drittstaaten (außerhalb der Europäischen Union, Island, Liechtenstein, Norwegen, also insbesondere in den USA) oder einer Datenweitergabe dorthin verwendet Adobe sogenannte Standardvertragsklauseln (= Art. 46. Abs. 2 und 3 DSGVO). Standardvertragsklauseln (Standard Contractual Clauses – SCC) sind von der EU-Kommission bereitgestellte Mustervorlagen und sollen sicherstellen, dass Ihre Daten auch dann den europäischen Datenschutzstandards entsprechen, wenn diese in Drittländer (wie beispielsweise in die USA) überliefert und dort gespeichert werden. Durch diese Klauseln verpflichtet sich Adobe, bei der Verarbeitung Ihrer relevanten Daten, das europäische Datenschutzniveau einzuhalten, selbst wenn die Daten in den USA gespeichert, verarbeitet und verwaltet werden. Diese Klauseln basieren auf einem Durchführungsbeschluss der EU-Kommission. Sie finden den Beschluss und die entsprechenden Standardvertragsklauseln u.a. hier: https://eur-lex.europa.eu/eli/dec_impl/2021/914/oj?locale=de</p>
+      <p>Mehr Informationen zu den verarbeiteten Daten und den Standardvertragsklauseln bei Adobe finden Sie unter https://www.adobe.com/de/privacy/eudatatransfers.html.</p>
+
+      <!-- Calendly -->
+      <h3>Calendly</h3>
+      <p>Wir verwenden für unsere Website Calendly, ein Planungs- und Organisationstool. Dienstanbieter ist das amerikanische Unternehmen Calendly LCC, 271 17th St NW, Ste 1000, Atlanta, Georgia, 30363, USA.</p>
+      <p>Calendly verarbeitet Daten u.a. auch in den USA. Wir weisen darauf hin, dass nach Meinung des Europäischen Gerichtshofs derzeit kein angemessenes Schutzniveau für den Datentransfer in die USA besteht. Dies kann mit verschiedenen Risiken für die Rechtmäßigkeit und Sicherheit der Datenverarbeitung einhergehen.</p>
+      <p>Als Grundlage der Datenverarbeitung bei Empfängern mit Sitz in Drittstaaten (außerhalb der Europäischen Union, Island, Liechtenstein, Norwegen, also insbesondere in den USA) oder einer Datenweitergabe dorthin verwendet Calendly von der EU-Kommission genehmigte Standardvertragsklauseln (= Art. 46. Abs. 2 und 3 DSGVO). Diese Klauseln verpflichten Calendly, das EU-Datenschutzniveau bei der Verarbeitung relevanter Daten auch außerhalb der EU einzuhalten. Diese Klauseln basieren auf einem Durchführungsbeschluss der EU-Kommission. Sie finden den Beschluss sowie die Klauseln u.a. hier: https://germany.representation.ec.europa.eu/index_de</p>
+      <p>Mehr über die Daten, die durch die Verwendung von Calendly verarbeitet werden, erfahren Sie in der Datenschutzerklärung auf https://calendly.com/de/privacy.</p>
+
+      <!-- Auskunft, Löschung, Sperrung, Speicherdauer -->
+      <h3>Auskunft, Löschung, Sperrung, Speicherdauer</h3>
+      <p>Ihre personenbezogenen Daten werden von uns nur so lange aufbewahrt, wie dies nach anwendbarem Recht zulässig ist. Wir speichern Ihre personenbezogenen Daten zudem jedenfalls so lange gesetzliche Aufbewahrungsfristen bestehen oder Verjährungsfristen potenzieller Rechtsansprüche noch nicht abgelaufen sind.</p>
+      <p>Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung sowie ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.</p>
+
+      <!-- Widerspruch Werbe-Mails -->
+      <h3>Widerspruch Werbe-Mails</h3>
+      <p>Der Nutzung von im Rahmen der Impressumspflicht und anderen veröffentlichten Kontaktdaten zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-E-Mails, vor.</p>
+
+      <p class="quelle">Quelle: https://www.e-recht24.de/muster-datenschutzerklaerung.html & https://www.adsimple.de/datenschutz-generator</p>
+    </div>
   </Layout>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  margin-block-start: 6rem;
+  text-align: center;
+}
+
+p + p,
+p + h3,
+p + ul,
+ul + p {
+  margin-block-start: 1em;
+}
+
+ul {
+  list-style-type: square;
+  list-style-position: inside;
+}
+
+ul {
+  color: var(--clr-text);
+  font-family: var(--ff-primary);
+  font-size: var(--fs-400);
+  font-style: italic;
+  line-height: 1.8;
+
+  @media (max-width: 1300px) {
+    line-height: 1.6;
+  }
+}
+
+.card {
+  background-image: url("../assets/images/index/hintergrund.png");
+  margin-block-end: 8rem;
+  margin-block-start: 4rem;
+  margin-inline: auto;
+  max-width: 800px;
+  padding: 3rem 3rem;
+  text-align: center;
+}
+
+.quelle {
+  font-size: var(--fs-100);
+}
+</style>
