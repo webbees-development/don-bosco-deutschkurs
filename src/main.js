8cffd58a281314eeec89cb9b0dbc8954439e7a38
stephan steiner <dev@webbees.at>
2023-03-20 23:42:50 +0100

    Update main.js
    configure infinite loader

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "~/assets/styles/global.scss";
+
 import DefaultLayout from '~/layouts/Default.vue'
+import InfiniteLoading from 'vue-infinite-loading'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  // Use the Ininite Loading package
+  Vue.use(InfiniteLoading)
 }
