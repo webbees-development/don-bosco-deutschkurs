b9ef939bc38f96894a051e6e5ab7b04d085b2dfa
stephan steiner <dev@webbees.at>
2023-04-06 00:24:23 +0200

    Update main.js
    comment LCP image in head

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "~/assets/styles/global.scss";
 
-import DefaultLayout from '~/layouts/Default.vue'
-import InfiniteLoading from 'vue-infinite-loading'
+import DefaultLayout from '~/layouts/Default.vue';
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
   // Use the Ininite Loading package
-  Vue.use(InfiniteLoading)
+  Vue.use(InfiniteLoading);
 
-  head.link.push({
-    rel: 'preload',
-    fetchpriority: "high",
-    as: "image",
-    href: "./assets/images/index/hintergrund.jpg",
-    type: "image/jpg",
-  })
+  // head.link.push({
+  //   rel: 'preload',
+  //   fetchpriority: "high",
+  //   as: "image",
+  //   href: "./assets/images/index/hintergrund.jpg",
+  //   type: "image/jpg",
+  // });
 }
