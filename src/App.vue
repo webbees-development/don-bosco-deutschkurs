751d9cb923dc7eaa297d8dc2aebccf40f11eeab3
stephan steiner <dev@webbees.at>
2023-04-06 03:10:29 +0200

    Create App.vue
    add an overwrite for App.vue to add transitions

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition>
+    <router-view />
+  </transition>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: this.$static.metadata.siteDescription
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
