894a2442c31bcc0fd20ee0c6dd6738c53a011268
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-05-29 19:27:57 +0200

    Update PostPreview.vue
    using padding hack instead of aspect ratio, delete unnecessary code

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -2,18 +2,20 @@
   <transition name="fade" appear>
     <g-link :to="link" class="preview">
       <figure>
-        <g-image
-          v-show="$useWebp === true" 
-          :class="['post-image', white_border ? 'white-border': '']" 
-          :src="image.src.webp" 
-          :alt="image.alt" 
-        />
-        <g-image
-          v-show="$useWebp === false" 
-          :class="['post-image', white_border ? 'white-border': '']" 
-          :src="image.src.source" 
-          :alt="image.alt" 
-        />
+        <div class="image-container">
+          <g-image
+            v-show="$useWebp === true"
+            :class="['post-image', white_border ? 'white-border' : '']"
+            :src="image.src.webp"
+            :alt="image.alt"
+          />
+          <g-image
+            v-show="$useWebp === false"
+            :class="['post-image', white_border ? 'white-border' : '']"
+            :src="image.src.source"
+            :alt="image.alt"
+          />
+        </div>
         <figcaption>
           <h3>{{ title }}</h3>
           <p class="excerpt">{{ excerpt }}...</p>
@@ -48,12 +50,12 @@ export default {
       type: String,
       required: true,
     },
-    white_border:{
+    white_border: {
       type: Boolean,
       default: false,
-    }
+    },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -61,11 +63,10 @@ export default {
 
 h3 {
   display: -webkit-box;
-  // margin-block-start: 2rem;
   margin-top: 2rem;
   overflow: hidden;
   -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;  
+  -webkit-box-orient: vertical;
   word-wrap: break-word;
 
   @media (min-width: $mobile-break) {
@@ -75,7 +76,6 @@ h3 {
 
 figcaption {
   max-width: min(80%, 50ch);
-  // margin-inline: auto;
   margin-left: auto;
   margin-right: auto;
 
@@ -87,11 +87,30 @@ figcaption {
   }
 }
 
+.image-container {
+  position: relative;
+  padding-top: 75%;
+}
+.post-image {
+  background-image: url("../assets/images/index/hintergrund.jpg");
+  border: 1.5rem solid transparent;
+  transition: transform 1000ms ease;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+
 p.excerpt {
   display: -webkit-box;
-  // margin-block-start: 0.5rem;
   margin-top: 0.5rem;
-  // margin-block-end: 1rem;
   margin-bottom: 1rem;
   overflow: hidden;
   -webkit-line-clamp: 3;
@@ -99,29 +118,14 @@ p.excerpt {
   word-wrap: break-word;
 }
 
-.post-image {
-  aspect-ratio: 1.4;
-  background-image: url("../assets/images/index/hintergrund.jpg");
-  border: 1.5rem solid transparent;
-  object-fit: cover;
-  transition: transform 1000ms ease;
-  width: 100%;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-}
-
 .preview {
   text-align: center;
 
   &:focus-visible {
     outline: none;
-
     .post-image {
       transform: scale(1.15);
     }
-
     .read-more {
       text-decoration: underline;
     }
@@ -145,7 +149,7 @@ p.excerpt {
   }
 }
 
-.white-border{
+.white-border {
   background: white;
 }
 </style>
